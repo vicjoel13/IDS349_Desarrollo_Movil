61202d7ae5c41d56ae978601e8d8a4c0b4a5972c
vicjoel13 <vicjoel13@gmail.com>
2023-11-24 18:48:12 -0400

    food shop

---
+++ FoodShop/lib/card3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Card3 extends StatelessWidget {
+  const Card3({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+  );
+  }
+}
